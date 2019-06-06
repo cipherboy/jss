aec5d040381ac5b59dfb7638041237ab27a597f9
Alexander Scheel <ascheel@redhat.com>
2019-06-06 15:23:56 -0400

    Move JSSContext to provider.javax.net.JSSContext
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- org/mozilla/jss/provider/javax/net/JSSContext.java
+++ org/mozilla/jss/provider/javax/net/JSSContext.java
@@ -1,4 +1,4 @@
-package org.mozilla.jss.ssl.javax;
+package org.mozilla.jss.provider.javax.net;
 
 import java.security.*;
 
@@ -9,31 +9,60 @@ import org.slf4j.LoggerFactory;
 
 import org.mozilla.jss.CryptoManager;
 import org.mozilla.jss.provider.javax.crypto.JSSKeyManager;
+import org.mozilla.jss.provider.javax.crypto.JSSTrustManager;
 import org.mozilla.jss.pkcs11.PK11Cert;
 import org.mozilla.jss.pkcs11.PK11PrivKey;
+import org.mozilla.jss.ssl.javax.JSSEngine;
 
 public class JSSContext extends SSLContextSpi {
     public static Logger logger = LoggerFactory.getLogger(JSSContext.class);
 
-    PK11Cert cert = null;
-    PK11PrivKey key = null;
+    JSSKeyManager key_manager = null;
+    JSSTrustManager trust_manager = null;
 
     public void engineInit(KeyManager[] km, TrustManager[] tm, SecureRandom sr) throws KeyManagementException {
         logger.debug("JSSContext: engineInit(" + km + ", " + tm + ", " + sr + ")");
 
-        throw new KeyManagementException("Multiple KeyManagers are not supported by the JSS SSLContext implementation");
+        if (km != null) {
+            for (KeyManager k : km) {
+                if (k instanceof JSSKeyManager) {
+                    key_manager = (JSSKeyManager) k;
+                    break;
+                }
+            }
+        }
+
+        if (tm != null) {
+            for (TrustManager t : tm) {
+                if (t instanceof JSSTrustManager) {
+                    trust_manager = (JSSTrustManager) t;
+                    break;
+                }
+            }
+        }
     }
 
     public SSLEngine engineCreateSSLEngine() {
         logger.debug("JSSContext: engineCreateSSLEngine()");
 
-        return new JSSEngine();
+        JSSEngine ret = new JSSEngine();
+        initializeEngine(ret);
+
+        return ret;
     }
 
     public SSLEngine engineCreateSSLEngine(String host, int port) {
         logger.debug("JSSContext: engineCreateSSLEngine(" + host + ", " + port + ")");
 
-        return new JSSEngine(host, port);
+        JSSEngine ret = new JSSEngine(host, port);
+        initializeEngine(ret);
+
+        return ret;
+    }
+
+    private void initializeEngine(JSSEngine eng) {
+        eng.setKeyManager(key_manager);
+        eng.setTrustManager(trust_manager);
     }
 
     public SSLSessionContext engineGetClientSessionContext() {
